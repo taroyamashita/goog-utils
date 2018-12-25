a3c1b94cc6fa9eb1cc97e14ed838221e42704794
Taro Yamashita <taro.c.yamashita@gmail.com>
2018-12-25 17:56:55 -0500

    configured to make workable

--- server/index.js
+++ server/index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
+});
+
+
 
 
 app.use(bodyParser.json());
