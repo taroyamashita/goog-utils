14bd38e235088c10497a25ab7f547b66de7c088c
Taro Yamashita <taro.c.yamashita@gmail.com>
2018-12-24 19:59:01 -0500

    add support for server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 let PORT = 7069 || process.env.PORT || 7069;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+
